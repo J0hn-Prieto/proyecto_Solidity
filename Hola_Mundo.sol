53c9f66d13a6c70bb3cdc1ef6b30a3b4312bc349
J0hn-Prieto <johnprieto1088@gmail.com>
2024-09-30 00:58:53 -0500

    Se crea primer contrato en Avalanche

---
+++ Hola_Mundo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Mi primer contrato hola mundo
+
+// 0xAD503235e6Cc21717E7D9732B20C71B76044f619
+
+pragma solidity ^0.8.24;
+
+contract hola_mundo {
+    string public saludo = "Hola mundo desde Avalanche Colombia";
+}
+
